573ab3249bd808702664b4c95412c65e4ef8537c
gdamiao <NB26858@novabase.pt>
2022-03-05 11:09:40 +0000

    FAQ page added + fixes

--- citizenfinance/src/app/app-routing.module.ts
+++ citizenfinance/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { faq } from './faq';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/faq', component: faq
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ citizenfinance/src/app/faq.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'faq',
+  templateUrl: './faq.html'
+})
+export class faq {
+  title = 'Citizen Finance';
+}
